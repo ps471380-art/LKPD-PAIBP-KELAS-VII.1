165da2fb0f2849f1def1847bdcbbce8d5b11f359
ps471380-art <ps471380@gmail.com>
2026-02-10 01:08:49 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,32 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var currentDate = new Date()
+var day = currentDate.getDate()
+var month = currentDate.getMonth() + 1
+var year = currentDate.getFullYear();
+var player = GetPlayer();
+var newName = day + "/" + month + "/" +year
+player.SetVar("DateValue", newName);
+}
+
+window.Script2 = function()
+{
+  window.print();
+}
+
+};
